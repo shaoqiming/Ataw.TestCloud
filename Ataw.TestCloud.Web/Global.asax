2f191b3be392a8e9b425a7f6fe2a54f8a62e7dc7
Shaoqi <549730265@qq.com>
2017-03-20 16:19:09 +0800

    初始化TestCloud项目

---
+++ Ataw.TestCloud.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ataw.TestCloud.Web.MvcApplication" Language="C#" %>
---
+++ Ataw.TestCloud.Web/TestCloudAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ataw.TestCloud.Web
+{
+    public class TestCloudAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "TestCloud";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+              "TestCloud_default",
+              "RightCloud/{controller}/{action}/{id}",
+              new { controller = "UI", action = "Home", id = UrlParameter.Optional },
+                new[] { "Ataw.RightCloud.Web", "Ataw.RightCloud.Web.Controllers" }
+
+          );
+        }
+    }
+}
