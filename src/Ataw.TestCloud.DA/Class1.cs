3b29fbb1c23ce0a3cb35bfd288437457687f7661
Shaoqi <549730265@qq.com>
2017-03-20 17:26:09 +0800

    创建基本的类库

---
+++ src/Ataw.TestCloud.DA/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ataw.TestCloud.DA
+{
+    public class Class1
+    {
+    }
+}
